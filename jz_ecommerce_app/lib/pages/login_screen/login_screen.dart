fa78b2ea66503fef7ac6d0b67aa4b0b9b167e62a
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-26 14:45:12 +0500

    26/02/2022 14:45:07

--- jz_ecommerce_app/lib/pages/login_screen/login_screen.dart
+++ jz_ecommerce_app/lib/pages/login_screen/login_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jz_ecommerce_app/Config/constants.dart';
 import 'package:jz_ecommerce_app/Config/theme.dart';
 import 'package:jz_ecommerce_app/components/button.dart';
@@ -8,6 +9,7 @@ import 'package:jz_ecommerce_app/components/input_feild.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jz_ecommerce_app/components/text_button.dart';
 import 'package:jz_ecommerce_app/components/text_feild.dart';
+import 'package:jz_ecommerce_app/pages/signup_screen/signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -19,6 +21,9 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  void Navigation() {
+    
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +32,7 @@ class _LoginScreenState extends State<LoginScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(child: Image.asset(logo_medium)),
+          SvgPicture.asset(logo_medium, semanticsLabel: 'Login Logo'),
           InputFeild(
               prefixIcon: Icons.person_rounded,
               controller: emailController,
@@ -46,15 +51,22 @@ class _LoginScreenState extends State<LoginScreen> {
             padding: const EdgeInsets.all(5),
             margin: const EdgeInsets.only(top: 20),
           ),
-          SizedBox(height:20.h),
-          DynamicTextFeild(text: 'Forget Password ?',style: dynamicTextStyle),
-          SizedBox(height:12.h),
+          SizedBox(height: 20.h),
+          DynamicTextFeild(text: 'Forget Password ?', style: dynamicTextStyle),
+          SizedBox(height: 12.h),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              DynamicTextFeild(text: 'dont have account?',style: secondaryTextStyle),
-              SizedBox(width: 10.w,),
-              DynamicTextButton(function: (){}, text: 'SignUp')
+              DynamicTextFeild(
+                  text: 'dont have account?', style: secondaryTextStyle),
+              SizedBox(
+                width: 10.w,
+              ),
+              DynamicTextButton(
+                  function: () {
+                    Navigation();
+                  },
+                  text: 'SignUp')
             ],
           ),
         ],
