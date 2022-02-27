8b78f83594f95c90c2f2c77b7a31932f97314310
Jaffar Abbas <gamaportal8@gmail.com>
2022-02-27 17:28:16 +0500

    27/02/2022 17:28:12

--- jz_ecommerce_app/lib/pages/home_screen/home_screen.dart
+++ jz_ecommerce_app/lib/pages/home_screen/home_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jz_ecommerce_app/Config/constants.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jz_ecommerce_app/Config/theme.dart';
+import 'package:jz_ecommerce_app/pages/home_screen/widgets/product_card.dart';
+import 'package:jz_ecommerce_app/pages/home_screen/widgets/product_grid.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,21 +12,31 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            width: double.infinity,
-            height: 235.h,
-            child: SvgPicture.asset(main_background,
-                semanticsLabel: 'Splash Logo', fit: BoxFit.cover),
-          ),
-          Positioned(
-            top: 50.h,
-            child: Container(
-              child: SvgPicture.asset(home_logo, semanticsLabel: 'Signup Logo')),
-          )
-        ],
+      body: Container(
+        padding: EdgeInsets.all(0),
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          fit: StackFit.expand,
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              top: 0,
+              child: Container(
+                child: SvgPicture.asset(main_background,
+                    semanticsLabel: 'Splash Logo', fit: BoxFit.fitHeight),
+              ),
+            ),
+            Positioned(
+              top: 60.h,
+              child: Container(
+                child:
+                    SvgPicture.asset(home_logo, semanticsLabel: 'Signup Logo'),
+              ),
+            ),
+            // ProductGrid(),
+          ],
+        ),
       ),
     );
   }
