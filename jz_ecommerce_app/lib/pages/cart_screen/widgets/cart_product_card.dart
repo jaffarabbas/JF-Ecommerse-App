d8068c2db27d83f717f6687ddf63bd3d91b0fa6d
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-03 20:02:53 +0500

    03/03/2022 20:02:53

---
+++ jz_ecommerce_app/lib/pages/cart_screen/widgets/cart_product_card.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:jz_ecommerce_app/Config/constants.dart';
+import 'package:jz_ecommerce_app/Config/theme.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jz_ecommerce_app/pages/cart_screen/widgets/cart_buttons.dart';
+import 'package:jz_ecommerce_app/pages/cart_screen/widgets/cart_product_info.dart';
+
+class CartProductCard extends StatefulWidget {
+  const CartProductCard({Key? key}) : super(key: key);
+
+  @override
+  _CartProductCardState createState() => _CartProductCardState();
+}
+
+class _CartProductCardState extends State<CartProductCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: CartCardDecoration,
+      margin: EdgeInsets.symmetric(horizontal:20.w,vertical:16.h),
+      height: 90.0.h,
+      width: 280.0.w,
+      child: Row(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+                color: kPrimaryColor,
+                size: 20.sp,
+              ),
+            ),
+          ),
+          Container(
+            height: 80.h,
+            width: 90.w,
+            child: Image.asset(
+              p2,
+              fit: BoxFit.scaleDown,
+            ),
+          ),
+          CartProdctInfo(),
+        ],
+      ),
+    );
+  }
+}
