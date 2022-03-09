f6b2d66f2d0a4f2eead12e538950cd1edf5657f1
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-09 19:01:03 +0500

    09/03/2022 19:00:59

--- jz_ecommerce_app/lib/pages/favourite_screen/widgets/favourite_header.dart
+++ jz_ecommerce_app/lib/pages/favourite_screen/widgets/favourite_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jz_ecommerce_app/Config/constants.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jz_ecommerce_app/Config/theme.dart';
 
 class FavouriteHeader extends StatefulWidget {
   const FavouriteHeader({Key? key}) : super(key: key);
@@ -21,21 +22,11 @@ class FavouriteHeaderState extends State<FavouriteHeader> {
         children: [
           Text(
             'FAVOURITE',
-            style: TextStyle(
-              color: kPrimaryLightColor,
-              fontSize: 19.sp,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 8.4.sp,
-            ),
+            style: HeaderMainHeadingTextStyle
           ),
           Text(
             'Total 5 items',
-            style: TextStyle(
-              color: kPrimaryLightColor,
-              fontSize: 9.sp,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 4.sp,
-            ),
+            style: HeaderPrimaryHeadingTextStyle,
           ),
         ],
       ),
