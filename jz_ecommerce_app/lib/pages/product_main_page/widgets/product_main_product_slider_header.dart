e207981bb64d3e5797ac8a8b83b2e9c89c071053
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-09 19:01:01 +0500

    09/03/2022 19:00:59

---
+++ jz_ecommerce_app/lib/pages/product_main_page/widgets/product_main_product_slider_header.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:jz_ecommerce_app/Config/constants.dart';
+
+class MainSliderHeader extends StatefulWidget {
+  const MainSliderHeader({Key? key}) : super(key: key);
+
+  @override
+  State<MainSliderHeader> createState() => _MainSliderHeaderState();
+}
+
+class _MainSliderHeaderState extends State<MainSliderHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.shopping_cart_rounded,
+            color: kPrimaryColor,
+            size: 30,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.favorite_outline_rounded,
+            color: kPrimaryColor,
+            size: 30,
+          ),
+        ),
+      ],
+    );
+  }
+}
