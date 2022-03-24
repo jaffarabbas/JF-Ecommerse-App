8c5bfeadfc5a12f0d9a02ec09f3c8302aa6b49eb
Jaffar Abbas <gamaportal8@gmail.com>
2022-03-24 21:19:33 +0500

    24/03/2022 21:19:31

--- jz_ecommerce_app/lib/main.dart
+++ jz_ecommerce_app/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       builder: () => MaterialApp(
         theme: theme(),
-        home: ProductMainPage(),
+        home: HomeScreen(),
         debugShowCheckedModeBanner: false,
       ),
       designSize: const Size(width,height),
