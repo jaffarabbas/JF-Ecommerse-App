448c6acb2d077a9fb2993876ea8ef0522cd4a4d4
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-04 23:30:46 +0500

    Commited File : product_main_slider_content.dart at 8/4/2022 11:30:45 PM

--- jz_ecommerce_app/lib/pages/product_main_page/widgets/product_main_slider_content.dart
+++ jz_ecommerce_app/lib/pages/product_main_page/widgets/product_main_slider_content.dart
@@ -4,6 +4,7 @@ import 'package:jz_ecommerce_app/Config/constants.dart';
 import 'package:jz_ecommerce_app/components/primaryImage.dart';
 import 'package:jz_ecommerce_app/pages/product_main_page/widgets/product_main_product_slider_header.dart';
 import 'package:jz_ecommerce_app/pages/product_main_page/widgets/product_main_slider_footer.dart';
+import 'package:jz_ecommerce_app/pages/product_main_page/widgets/product_main_slider_product_info.dart';
 
 class ProductSliderContent extends StatelessWidget {
   const ProductSliderContent({Key? key}) : super(key: key);
@@ -13,9 +14,9 @@ class ProductSliderContent extends StatelessWidget {
     return Column(
       children: [
         const MainSliderHeader(),
+        SizedBox(height: 10.h),
         PrimaryImage(image: p2, width: 180.w, height: 100.h),
-        SizedBox(height: 5.h),
-        const ProductSliderContent(),
+        ProductInfo(),
         SizedBox(height: 10.h),
         const ProductMainFooter(),
       ],
