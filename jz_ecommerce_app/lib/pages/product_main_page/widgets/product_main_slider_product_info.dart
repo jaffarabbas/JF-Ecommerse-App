989442cb4b23fdda198b6df5941f902953b074f6
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-05 19:08:07 +0500

    Commited File : product_main_slider_product_info.dart at 8/5/2022 7:08:06 PM

--- jz_ecommerce_app/lib/pages/product_main_page/widgets/product_main_slider_product_info.dart
+++ jz_ecommerce_app/lib/pages/product_main_page/widgets/product_main_slider_product_info.dart
@@ -3,7 +3,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jz_ecommerce_app/Config/theme.dart';
 
 class ProductInfo extends StatelessWidget {
-  const ProductInfo({Key? key}) : super(key: key);
+  String productName;
+  String category;
+  ProductInfo({Key? key,required this.productName , required this.category }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +15,11 @@ class ProductInfo extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Jorden\nShoes',
+            Text(productName,
                 style: ProductMainCardBottomCardMainHeadingStyle),
             SizedBox(height: 5.h),
             Text(
-              'Mens Runner',
+              category,
               style: ProductMainCardSubHeadingStyle,
             ),
           ],
