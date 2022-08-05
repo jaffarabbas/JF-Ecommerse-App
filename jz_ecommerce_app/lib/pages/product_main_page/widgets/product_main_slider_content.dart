abd4a6e70f4b74e37fc468ae34dc128767a00cfb
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-05 19:08:05 +0500

    Commited File : product_main_slider_content.dart at 8/5/2022 7:08:04 PM

--- jz_ecommerce_app/lib/pages/product_main_page/widgets/product_main_slider_content.dart
+++ jz_ecommerce_app/lib/pages/product_main_page/widgets/product_main_slider_content.dart
@@ -5,9 +5,11 @@ import 'package:jz_ecommerce_app/components/primaryImage.dart';
 import 'package:jz_ecommerce_app/pages/product_main_page/widgets/product_main_product_slider_header.dart';
 import 'package:jz_ecommerce_app/pages/product_main_page/widgets/product_main_slider_footer.dart';
 import 'package:jz_ecommerce_app/pages/product_main_page/widgets/product_main_slider_product_info.dart';
+import '../../../Model/testModel.dart';
 
 class ProductSliderContent extends StatelessWidget {
-  const ProductSliderContent({Key? key}) : super(key: key);
+  Product products;
+  ProductSliderContent({Key? key,required this.products}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +17,8 @@ class ProductSliderContent extends StatelessWidget {
       children: [
         const MainSliderHeader(),
         SizedBox(height: 10.h),
-        PrimaryImage(image: p2, width: 180.w, height: 100.h),
-        ProductInfo(),
+        PrimaryImage(image: products.image.toString(), width: 180.w, height: 100.h),
+        ProductInfo(productName: products.name.toString(),category: products.category.toString()),
         SizedBox(height: 10.h),
         const ProductMainFooter(),
       ],
